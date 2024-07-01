8901bf93aeff16e7a076d934c0b05e00cd611004
nosho.hiroaki <nosho404@gmail.com>
2024-07-01 20:34:57 +0900

    implements scaffold

--- app/src/main/java/com/example/publicplayground/MainActivity.kt
+++ app/src/main/java/com/example/publicplayground/MainActivity.kt
@@ -1,30 +1,92 @@
 package com.example.publicplayground
 
-import AnimationPlayground
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.publicplayground.ui.theme.PublicPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
             PublicPlaygroundTheme {
-                // A surface container using the 'background' color from the theme
+                val view = LocalView.current
+                if (!view.isInEditMode) {
+                    SideEffect {
+                        (view.context as Activity).window.statusBarColor =
+                            Color.Transparent.toArgb()
+                    }
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AnimationPlayground()
+                    PublicPlaygroundApp()
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PublicPlaygroundApp() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Public Playground @hiroaki404")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(Color.Cyan),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier,
+                text = "top"
+            )
+            Text(
+                modifier = Modifier,
+                text = "bottom"
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun EdgeToEdgePlaygroundPreview() {
+    PublicPlaygroundTheme {
+        PublicPlaygroundApp()
     }
 }
