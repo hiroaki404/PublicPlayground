8100736e2c2243f2bce7a012be771d9d484037b2
nosho.hiroaki <nosho404@gmail.com>
2024-07-08 17:37:08 +0900

    remove unused library

--- feature/animation/build.gradle.kts
+++ feature/animation/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
