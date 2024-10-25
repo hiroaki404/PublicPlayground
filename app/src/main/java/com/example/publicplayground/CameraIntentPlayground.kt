a784ddd440aee65bf714fb2c462984ac6b1349ab
hiroaki404 <nosho404@gmail.com>
2024-10-25 18:41:18 +0900

    save file

--- app/src/main/java/com/example/publicplayground/CameraIntentPlayground.kt
+++ app/src/main/java/com/example/publicplayground/CameraIntentPlayground.kt
@@ -1,10 +1,15 @@
 package com.example.publicplayground
 
-import android.content.Context
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
+import android.media.ExifInterface
+import android.os.Build
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -13,42 +18,53 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.FileProvider
 import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Objects
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun CameraIntentPlayground(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    val file = context.createImageFile()
-    val uri = FileProvider.getUriForFile(
-        Objects.requireNonNull(context),
-        BuildConfig.APPLICATION_ID + ".provider", file
-    )
+
+    val file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "IMG.jpg")
+    val uri =
+        FileProvider.getUriForFile(context, "${BuildConfig.APPLICATION_ID}.fileprovider", file)
 
     val launcher =
         rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, "sample")
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val uri = context.contentResolver.insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+                )
+                ExifInterface(file).apply {
+                    // その他の GPS 情報 (オプション)
+                    setAttribute(ExifInterface.TAG_GPS_LATITUDE, "0/0,0/0000,00000000/00000");
+                    setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, "0");
+                    setAttribute(ExifInterface.TAG_GPS_LONGITUDE, "0/0,0/0,000000/00000 ");
+                    setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, "0");
+                    saveAttributes()
+                }
+                context.contentResolver.openOutputStream(uri!!).use { output ->
+                    file.inputStream().use { input ->
+                        input.copyTo(output!!)
+                    }
+                }
+            }
         }
 
     Column(modifier = modifier) {
         Button(onClick = {
-            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-            intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5)
+//            intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5)
+
             launcher.launch(intent)
         }) {
             Text("Pick Content")
         }
     }
-}
-
-fun Context.createImageFile(): File {
-    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-    val imageFileName = "JPEG_" + timeStamp + "_"
-    val image = File.createTempFile(
-        imageFileName,
-        ".jpg",
-        externalCacheDir
-    )
-    return image
 }
