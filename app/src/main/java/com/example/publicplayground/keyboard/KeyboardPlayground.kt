1bd624c2440e399e79f7a64d31f4d1247b5b204a
hiroaki404 <nosho404@gmail.com>
2024-12-21 14:31:26 +0900

    add keyboard playground

---
+++ app/src/main/java/com/example/publicplayground/keyboard/KeyboardPlayground.kt
@@ -0,0 +1,108 @@
+package com.example.publicplayground.keyboard
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun KeyboardPlayground(modifier: Modifier = Modifier) {
+    val focusRequester = remember { FocusRequester() }
+    val interactionSource = remember { MutableInteractionSource() }
+    Column(
+        modifier = modifier
+            .imePadding()
+            .verticalScroll(rememberScrollState())
+            .focusRequester(focusRequester)
+            .focusable()
+            .clickable(interactionSource = interactionSource, indication = null) {
+                focusRequester.requestFocus()
+            }
+    ) {
+        AllKeyboardTypeTextField()
+        AllImeActionTextField()
+    }
+}
+
+@Composable
+fun AllKeyboardTypeTextField() {
+    Text(text = "KeyboardType", style = MaterialTheme.typography.titleMedium)
+    HorizontalDivider()
+
+    listOf(
+        KeyboardType.Unspecified,
+        KeyboardType.Text,
+        KeyboardType.Ascii,
+        KeyboardType.Number,
+        KeyboardType.Phone,
+        KeyboardType.Uri,
+        KeyboardType.Email,
+        KeyboardType.Password,
+        KeyboardType.NumberPassword,
+        KeyboardType.Decimal,
+    ).forEach {
+        Text("$it")
+        var text by remember { mutableStateOf("") }
+
+        TextField(
+            value = text,
+            singleLine = true,
+            onValueChange = { text = it },
+            keyboardOptions = KeyboardOptions(keyboardType = it),
+        )
+    }
+}
+
+@Composable
+fun AllImeActionTextField() {
+    Text(text = "KeyboardAction", style = MaterialTheme.typography.titleMedium)
+    HorizontalDivider()
+
+    listOf(
+        ImeAction.Unspecified,
+        ImeAction.Default,
+        ImeAction.None,
+        ImeAction.Go,
+        ImeAction.Search,
+        ImeAction.Send,
+        ImeAction.Previous,
+        ImeAction.Next,
+        ImeAction.Done,
+    ).forEach {
+        Text("$it")
+        var text by remember { mutableStateOf("") }
+
+        TextField(
+            value = text,
+            singleLine = true,
+            onValueChange = { text = it },
+            keyboardOptions = KeyboardOptions(imeAction = it),
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun KeyboardPlaygroundPreview() {
+    KeyboardPlayground()
+}
