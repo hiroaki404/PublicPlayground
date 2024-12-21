3099545659161241fada4cee5b24078e91401db4
hiroaki404 <nosho404@gmail.com>
2024-12-21 22:08:13 +0900

    update preview

--- app/src/main/java/com/example/publicplayground/keyboard/KeyboardPlayground.kt
+++ app/src/main/java/com/example/publicplayground/keyboard/KeyboardPlayground.kt
@@ -1,5 +1,6 @@
 package com.example.publicplayground.keyboard
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -22,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -107,8 +109,8 @@ fun AllImeActionTextField() {
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, heightDp = 2000)
 @Composable
 fun KeyboardPlaygroundPreview() {
-    KeyboardPlayground()
+    KeyboardPlayground(modifier = Modifier.padding(16.dp).border(1.dp, Color.Gray))
 }
